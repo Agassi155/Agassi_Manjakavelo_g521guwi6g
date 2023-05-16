ac67e6d6e15383504d87f24acb78f6bee5469e81
agassi <agassi@mail.com>
2023-05-16 20:16:57 +0300

    feat: use DTO for request and responses for jwt

---
+++ src/main/java/com/test/security/dto/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.test.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+
+}
